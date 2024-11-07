3188c9fdef4989530e3d79511b9b4305cb0712eb
myogeon-ai <myogeon.ai@gmail.com>
2024-11-07 16:04:38 +0900

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -1,10 +1,3 @@
 # pip install langchain
 import streamlit as st
 from langchain_community.llms import OpenAI
-
-
-st.title('🍎🍐🍊 나의 AI Chat 🥝🍅🍆')
-
-# openai_api_key = st.sidebar.text_input('OpenAI API Key')
-openai_api_key = st.secrets["openai"]["api_key"]
-
